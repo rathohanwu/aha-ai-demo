7341cdf1328195abd194c0676d15de3a5a579412
Rathohan Wu <rathohanwu@gmail.com>
2023-12-19 20:37:10 +0800

    updating cors config

--- services/api-service/src/main.ts
+++ services/api-service/src/main.ts
@@ -11,33 +11,48 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('AHA Demo API Example')
-    .setDescription('AHA Demo API Example')
-    .setVersion('1.0')
-    .addTag('AHA Demo API')
-    .addBearerAuth(
-      {
-        description: 'Enter the Auth Token',
-        name: 'Authorization',
-        bearerFormat: 'Bearer',
-        scheme: 'Bearer',
-        type: 'http',
-        in: 'Header',
-      },
-      'access-token'
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  // swagger
+  const swaggerDocument = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('doc', app, swaggerDocument);
+
   app.use(activeSessionTracker);
   app.use(cookieParser());
-  app.enableCors({
-    origin: [process.env.CLIENT_URL],
-    credentials: true,
-  });
+
+  app.enableCors(corsConfig);
   await prisma.$connect();
   await app.listen(3020);
 }
+
+const corsConfig = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Authorization',
+  ],
+  methods: ['GET', 'POST', 'PATCH'],
+};
+
+const documentConfig = new DocumentBuilder()
+  .setTitle('AHA Demo API Example')
+  .setDescription('AHA Demo API Example')
+  .setVersion('1.0')
+  .addTag('AHA Demo API')
+  .addBearerAuth(
+    {
+      description: 'Enter the Auth Token',
+      name: 'Authorization',
+      bearerFormat: 'Bearer',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'Header',
+    },
+    'access-token'
+  )
+  .build();
 
 bootstrap();
