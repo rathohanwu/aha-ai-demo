118a24654ca86ba6ae8d4f78863f8a9d88949795
Rathohan Wu <rathohanwu@gmail.com>
2023-12-19 16:52:21 +0800

    adding docker file for frontend

--- clients/aha-ai-demo/src/types/account.ts
+++ clients/aha-ai-demo/src/types/account.ts
@@ -1,13 +1,13 @@
 type Account = {
-    name: string,
-    email: string
-    signUpTime: string,
-    activeTime: string,
-    verified: boolean,
-    _count: {
-        logins: number
-    }
-}
+  name: string;
+  email: string;
+  signUpTime: string;
+  activeTime: string;
+  verified: boolean;
+  _count: {
+    logins: number;
+  };
+};
 
 // eslint-disable-next-line prettier/prettier
 export {type Account}
